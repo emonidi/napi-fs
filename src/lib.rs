400cd794fa5bcc851355ef39624c098a1ff3c28d
Emiliyan Gospodinov <emonidi@gmail.com>
2024-04-14 13:23:20 +0200

    first commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
 #![deny(clippy::all)]
 
+
+use napi::bindgen_prelude::*;
 use napi_derive::napi;
+use tokio::fs;
 
 #[napi]
 pub fn plus_100(input: u32) -> u32 {
   input + 100
+}
+
+#[napi]
+async fn ensure_dir(dir:String) -> Result<()>{
+  tokio::fs::create_dir_all(dir).await;
+  Ok(())
 }
